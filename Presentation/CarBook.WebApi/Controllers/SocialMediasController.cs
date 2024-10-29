c590a6ead4e49b8f22eaa1596f9391436179b33c
halilselvitopu <halilselvitopu@gmail.com>
2024-10-29 18:21:59 +0300

    SocialMedia mediator, API and UI theme added

---
+++ Presentation/CarBook.WebApi/Controllers/SocialMediasController.cs
@@ -0,0 +1,54 @@
+﻿using CarBook.Application.Features.Mediator.Commands.SoialMediaCommands;
+using CarBook.Application.Features.Mediator.Queries.SocialMediaQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialMediasController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SocialMediasController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SocialMediaList()
+        {
+            return Ok(await _mediator.Send(new GetSocialMediaQuery()));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSocialMediaById(int id)
+        {
+            var value = await _mediator.Send(new GetSocialMediaByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSocialMedia(CreateSocialMediaCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Başarıyla Eklendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveSocialMedia(RemoveSocialMediaCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Başarıyla Silindi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateSocialMedia(UpdateSocialMediaCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Başarıyla Güncellendi");
+        }
+    }
+}
