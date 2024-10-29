d93d2ba7722611bec7b05266ed84d10d3c75ea96
halilselvitopu <halilselvitopu@gmail.com>
2024-10-29 23:28:58 +0300

    View Components for UI Layout

---
+++ Frontends/CarBook.WebUI/ViewComponents/UILayoutViewComponents/UILayoutLoaderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class UILayoutLoaderViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
