576c0e3a57849df2e1d5475e9b3aad16c370f888
halilselvitopu <halilselvitopu@gmail.com>
2024-10-31 02:11:04 +0300

    Services, car list, contact consume and send message

---
+++ Frontends/CarBook.WebUI/ViewComponents/FooterContactViewComponents/FooterContactViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using CarBook.Dto.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.FooterAdressViewComponents
+{
+    public class FooterContactViewComponent : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FooterContactViewComponent(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7127/api/FooterContacts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterContactDto>>(jsonData);
+                return View(values);
+
+            }
+            return View();
+        }
+    }
+}
