d0a2f5f249d34fa9e996402360596b4c912911a2
halilselvitopu <halilselvitopu@gmail.com>
2024-11-04 23:42:05 +0300

    up tag cloud

--- Core/CarBook.Application/Features/Mediator/Handlers/TagCloudHandlers/GetTagCloudByIdQueryHandler.cs
+++ Core/CarBook.Application/Features/Mediator/Handlers/TagCloudHandlers/GetTagCloudByIdQueryHandler.cs
@@ -1,33 +1,33 @@
-﻿//using CarBook.Application.Features.Mediator.Queries.TagCloudQueries;
-//using CarBook.Application.Features.Mediator.Results.TagClouds;
-//using CarBook.Application.Interfaces;
-//using CarBook.Application.Interfaces.BlogInterfaces;
-//using CarBook.Domain.Entities;
-//using MediatR;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using CarBook.Application.Features.Mediator.Queries.TagCloudQueries;
+using CarBook.Application.Features.Mediator.Results.TagClouds;
+using CarBook.Application.Interfaces;
+using CarBook.Application.Interfaces.BlogInterfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace CarBook.Application.Features.Mediator.Handlers.TagCloudHandlers
-//{
-//    public class GetTagCloudByIdQueryHandler : IRequestHandler<GetTagCloudByIdQuery, GetTagCloudByIdQueryResult>
-//    {
-//        private readonly IRepository<TagCloud> _repository;
-//        public GetTagCloudByIdQueryHandler(IRepository<TagCloud> repository)
-//        {
-//            _repository = repository;
-//        }
+namespace CarBook.Application.Features.Mediator.Handlers.TagCloudHandlers
+{
+    public class GetTagCloudByIdQueryHandler : IRequestHandler<GetTagCloudByIdQuery, GetTagCloudByIdQueryResult>
+    {
+        private readonly IRepository<TagCloud> _repository;
+        public GetTagCloudByIdQueryHandler(IRepository<TagCloud> repository)
+        {
+            _repository = repository;
+        }
 
-//        public async Task<GetTagCloudByIdQueryResult> Handle(GetTagCloudByIdQuery request, CancellationToken cancellationToken)
-//        {
-//            var value = await _repository.GetByIdAsync(request.Id);
-//            return new GetTagCloudByIdQueryResult
-//            {
-//                Id = value.Id,
-//                Title = value.Title,
-//            };
-//        }
-//    }
-//}
+        public async Task<GetTagCloudByIdQueryResult> Handle(GetTagCloudByIdQuery request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.Id);
+            return new GetTagCloudByIdQueryResult
+            {
+                Id = value.Id,
+                Title = value.Title,
+            };
+        }
+    }
+}
