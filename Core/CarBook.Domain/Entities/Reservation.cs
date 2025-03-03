07019ff85c2ef03e8aff3b8d840a0364157246a4
halilselvitopu <halilselvitopu@gmail.com>
2025-03-03 17:07:12 +0300

    Reservation entity, form and relation with location

---
+++ Core/CarBook.Domain/Entities/Reservation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int? PickUpLocationId { get; set; }
+        public int? DropOffLocationId { get; set; }
+        public int CarId { get; set; }
+        public int Age { get; set; }
+        public int DriverLicenseYear { get; set; }
+        public string? Description { get; set; }
+        public Location PickUpLocation { get; set; }
+        public Location DropOffLocation { get; set; }
+    }
+}
---
+++ Infrastructure/CarBook.Persistence/Migrations/20250303135749_mig_add_location_reservation_relation.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarBook.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_location_reservation_relation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PickUpLocationId = table.Column<int>(type: "int", nullable: true),
+                    DropOffLocationId = table.Column<int>(type: "int", nullable: true),
+                    CarId = table.Column<int>(type: "int", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    DriverLicenseYear = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Locations_DropOffLocationId",
+                        column: x => x.DropOffLocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Reservations_Locations_PickUpLocationId",
+                        column: x => x.PickUpLocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_DropOffLocationId",
+                table: "Reservations",
+                column: "DropOffLocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_PickUpLocationId",
+                table: "Reservations",
+                column: "PickUpLocationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservations");
+        }
+    }
+}
